3d596d39e17b6ca539035d506244b726af415962
NightDuck <53341294+NightDucks@users.noreply.github.com>
2021-04-26 10:39:45 +0200

    Created server and start script | added prettier

---
+++ src/server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+
+app.listen(3000, () => {
+  console.log("Started on http://localhost:3000");
+})
